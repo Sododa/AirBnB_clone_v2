5713e58385094994f4c9c12e0505778347d7c84f
Sododa <sophieododa@gmail.com>
2024-04-17 10:25:05 +0000

    trial

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,23 +1,49 @@
-#!/usr/bin/python3
-""" unittests for models"""
-import os
-from tests.test_models.test_base_model import test_basemodel
+import unittest
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 from models.state import State
 
+class TestState(unittest.TestCase):
+    """Test cases for State class"""
 
-class test_state(test_basemodel):
-    """test for State class """
+    @classmethod
+    def setUpClass(cls):
+        """Set up class-wide test environment"""
+        cls.storage = FileStorage()
+        cls.storage.reload()
 
-    def __init__(self, *args, **kwargs):
-        """test class for State """
-        super().__init__(*args, **kwargs)
-        self.name = "State"
-        self.value = State
+    @classmethod
+    def tearDownClass(cls):
+        """Tear down class-wide test environment"""
+        cls.storage.save()
 
-    def test_name3(self):
-        """Tests name attribute """
-        new = self.value()
-        self.assertEqual(
-                type(new.name),
-                str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
-                )
+    def test_state_exists(self):
+        """Test if a State object exists in storage"""
+        state = State(name="California")
+        self.storage.new(state)
+        self.storage.save()
+        key = "State.{}".format(state.id)
+        self.assertIn(key, self.storage.all())
+
+    @unittest.skipIf(True, "Skipping MySQL test")
+    def test_state_exists_mysql(self):
+        """Test if a State object exists in MySQL storage"""
+        from models.engine.db_storage import DBStorage
+
+        HBNB_MYSQL_USER = "hbnb_test"
+        HBNB_MYSQL_PWD = "hbnb_test_pwd"
+        HBNB_MYSQL_HOST = "localhost"
+        HBNB_MYSQL_DB = "hbnb_test_db"
+
+        storage = DBStorage()
+        storage.reload()
+
+        state = State(name="California")
+        storage.new(state)
+        storage.save()
+
+        key = "State.{}".format(state.id)
+        self.assertIn(key, storage.all())
+
+if __name__ == "__main__":
+    unittest.main()
