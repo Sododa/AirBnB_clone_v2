ae486dd4d03c12b9630f4a4f20a6bdee4e7d6a28
Sododa <sophieododa@gmail.com>
2024-04-17 16:05:47 +0000

    airbnb

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -4,7 +4,6 @@ def test_name_attribute(self):
     """
     # Create a State instance
     new_state = State(name="California")
-    
     if os.getenv('HBNB_TYPE_STORAGE') != 'db':
         # For non-DB storage, 'name' should be of type str
         self.assertIsInstance(new_state.name, str)
@@ -13,13 +12,13 @@ def test_name_attribute(self):
         # For DB storage, 'name' should initially be None
         self.assertIsNone(new_state.name)
 
+
 def test_save_method(self):
     """
     Test the 'save' method of the State instance
     """
     # Create a State instance
     new_state = State(name="New York")
-    
     # Save the State instance and check if 'updated_at' changes
     initial_updated_at = new_state.updated_at
     new_state.save()
