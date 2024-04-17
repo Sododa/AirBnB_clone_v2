c161227f7e8d4e9147b17ba31e5122a7ecf115b1
Sododa <sophieododa@gmail.com>
2024-04-17 15:36:31 +0000

    test models

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
-"""
-Defines the unittests for models/state.py
-"""
-import os
-from tests.test_models.test_base_model import TestBasemodel
+""" """
+from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
 
-class TestState(TestBasemodel):
-    """
-    A unittest for State class
-    """
+class test_state(test_basemodel):
+    """ """
 
     def __init__(self, *args, **kwargs):
-        """
-        Initializes the test class for State
-        """
+        """ """
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """
-        Tests the type of name attribute
-        """
-        new = self.value()
-        if os.getenv('HBNB_TYPE_STORAGE') != 'db':
-            self.assertEqual(type(new.name), str)
-        else:
-            self.assertIsNone(new.name)
+        """ """
+        new = self.value(name="test")
+        self.assertEqual(type(new.name), str)
