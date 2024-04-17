d9a4ec3624855e29d141b672693ce703aa2266ab
Sododa <sophieododa@gmail.com>
2024-04-17 11:07:48 +0000

    repositoty

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,51 +1,31 @@
-import unittest
-from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
+#!/usr/bin/python3
+"""
+Defines the unittests for models/test_state.py
+"""
+import os
+from tests.test_models.test_base_model import TestBasemodel
 from models.state import State
 
 
-class TestState(unittest.TestCase):
-    """Test cases for State class"""
+class TestState(TestBasemodel):
+    """
+    A unittest for State class
+    """
 
-    @classmethod
-    def setUpClass(cls):
-        """Set up class-wide test environment"""
-        cls.storage = FileStorage()
-        cls.storage.reload()
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes the test class for State
+        """
+        super().__init__(*args, **kwargs)
+        self.name = "State"
+        self.value = State
 
-    @classmethod
-    def tearDownClass(cls):
-        """Tear down class-wide test environment"""
-        cls.storage.save()
-
-    def test_state_exists(self):
-        """Test if a State object exists in storage"""
-        state = State(name="California")
-        self.storage.new(state)
-        self.storage.save()
-        key = "State.{}".format(state.id)
-        self.assertIn(key, self.storage.all())
-
-    @unittest.skipIf(True, "Skipping MySQL test")
-    def test_state_exists_mysql(self):
-        """Test if a State object exists in MySQL storage"""
-        from models.engine.db_storage import DBStorage
-
-        HBNB_MYSQL_USER = "hbnb_test"
-        HBNB_MYSQL_PWD = "hbnb_test_pwd"
-        HBNB_MYSQL_HOST = "localhost"
-        HBNB_MYSQL_DB = "hbnb_test_db"
-
-        storage = DBStorage()
-        storage.reload()
-
-        state = State(name="California")
-        storage.new(state)
-        storage.save()
-
-        key = "State.{}".format(state.id)
-        self.assertIn(key, storage.all())
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_name3(self):
+        """
+        Tests the type of name attribute
+        """
+        new = self.value()
+        self.assertEqual(
+            type(new.name),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
