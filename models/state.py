6afa0b9db4b4c921da1b14a10beed6c549419d5b
Sododa <sophieododa@gmail.com>
2024-05-19 19:44:33 +0000

    airbnb

--- models/state.py
+++ models/state.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
 """ holds class State"""
-import models
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from models.city import City
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+import models
 
 
 class State(BaseModel, Base):
-    """Representation of state """
-    if models.storage_t == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
-    else:
-        name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
 
     def __init__(self, *args, **kwargs):
         """initializes state"""
         super().__init__(*args, **kwargs)
 
-    if models.storage_t != "db":
         @property
         def cities(self):
             """getter for list of city instances related to the state"""
